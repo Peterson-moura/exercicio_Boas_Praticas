915ee8c08e90fa8526d3718d1abcc705ec706fc7
Peterson-moura <peterson.moura@outlook.com.br>
2021-12-01 20:46:22 -0300

    _inicial_exercicio_boasPraticas

---
+++ exercicios.boasPraticas/src/main/java/Peterson/moura/exercicios/boasPraticas/Application.java
@@ -0,0 +1,13 @@
+package Peterson.moura.exercicios.boasPraticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ exercicios.boasPraticas/src/test/java/Peterson/moura/exercicios/boasPraticas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Peterson.moura.exercicios.boasPraticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
